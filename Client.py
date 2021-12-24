4859b39702822b5c4575852cde38ad03d03df742
Ehud <ehudcode@gmail.com>
2021-12-24 22:05:54 +0200

    updates

--- Client.py
+++ Client.py
@@ -1,3 +1,5 @@
+import struct
+
 import scapy
 import socket
 
@@ -5,10 +7,12 @@ import socket
 
 
 class Client:
-    def __init__(self,ip_address, port):
+    def __init__(self,ip_address, port,team_name):
         self.ip_address = ip_address
         self.port = port
         self.create_udp_socket()
+        self.team_name = team_name
+        print("Client started, listening for offer requests...")
 
 
 
@@ -17,10 +21,23 @@ class Client:
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.udp_socket.bind(("",self.port))
-        while True:
-            data, addr = self.udp_socket.recvfrom(1024)
-            print(data)
+        message, address = self.udp_socket.recvfrom(1024)
+        print(f"Received offer from {address[0]},attempting to connect...")
+        self.connect(message,address)
+
+    def connect(self,message,address):
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        magic_cookie, message_type, server_port = struct.unpack(">IbH",message)
+        print(address[0])
+        self.tcp_socket.connect((socket.gethostbyname(socket.gethostname())d,server_port))
+        self.tcp_socket.send(f"{self.team_name}\n")
+    #     self.play()
+    #
+    # def play(self):
 
 
 
-s = Client(1,13117)
+
+
+
+s = Client(1,13117,"sdfa")
--- Server.py
+++ Server.py
@@ -1,9 +1,11 @@
+import struct
+
 import scapy
 import socket
 import time
+import threading
 
-
-
+from scapy.arch import get_if_addr
 
 Host = '192.168.0.1'
 port = 9376 #listening port
@@ -12,35 +14,72 @@ port = 9376 #listening port
 class Server():
 
 
-    def __init__(self,ip_address,broadcast_port,destination_port=13117):
+    def __init__(self,ip_address,tcp_port,broadcast_port,destination_port=13117):
         self.ip = ip_address
+        print(ip_address)
+        print(f"Server started, listening on IP address {self.ip}")
         self.broadcast_port = broadcast_port
         self.destination_port = destination_port
+        self.tcp_port = tcp_port
+        self.number_of_clients = 0
+        self.lock = threading.Lock()
+        self.event_udp = threading.Event()
+        self.event_tcp = threading.Event()
+        thread = threading.Thread(target=self.create_broadcast_socket)
+        thread.start()
 
-        self.create_broadcast_socket()
-        # create_tcp_listening_socket()
+        self.create_tcp_listening_socket()
 
 
 
     def create_broadcast_socket(self):
         # self.bind()
-
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        message = struct.pack(">IbH",0xabcddcba, 0x2, self.tcp_port)
+        self.send_message(message)
 
 
     def send_message(self,message):
         while True:
             self.udp_socket.sendto(message, ('<broadcast>', self.destination_port))
-            print("sent")
+            # print("sent")
             time.sleep(1)
+            if self.number_of_clients >= 2:
+                self.event_udp.wait()
+
+    def create_tcp_listening_socket(self):
+        self.tcp_listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.tcp_listener.bind(('127.0.0.1',self.tcp_port))
+        while True:
+            self.tcp_listener.listen()
+            connection, address = self.tcp_listener.accept() #should we accept him?
+            self.lock.acquire()
+            if self.number_of_clients < 2:
+                self.number_of_clients += 1
+                thread = threading.Thread(target=self.handle_client, args=(connection))
+                thread.start()
+                self.lock.release()
+            else:
+                self.lock.release()
+                connection.send("Sorry, a game is on...")
+                connection.send("go play some football")
+                connection.send("bye bye")
+                connection.close()
+                self.event_tcp.wait()
 
 
 
-s = Server(1,2000)
-s.send_message((b"asdfasdfsdf"))
+
+    def handle_client(self,connection):
+        print(connection.recv(1024))
+
+
+
+s = Server(socket.gethostbyname(socket.gethostname()),2500,broadcast_port=2000)
+
 
 
 
